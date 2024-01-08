7273d8eea156cdc77aa9eda14e0bb0c32e00381a
Michael Robinson <mike@pagesofinterest.net>
2024-01-08 21:40:09 +1300

    Updated colourscheme and pencil usage

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,14 @@
 return {
-  { "RRethy/nvim-base16" },
+  { "RRethy/nvim-base16", lazy = false },
+  { "preservim/vim-colors-pencil", lazy = false },
+  { "bluz71/vim-moonfly-colors", lazy = false },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "base16-bright",
+      -- colorscheme = "base16-bright",
+      -- colorscheme = "pencil",
+      colorscheme = "moonfly",
     },
   },
 }
